7e58628cacaf352dbd156e3bab11fd484b65d85b
ambujsahu81 <Ambuj.sahu1998@gmail.com>
2022-12-13 18:44:50 +0530

    initial commit

---
+++ dist/background.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";let t=!1;function e(t){document.body.style.backgroundColor=t}chrome.action.onClicked.addListener((c=>{t=!t;const n=t?"orange":"white";chrome.scripting.executeScript({target:{tabId:c.id?c.id:-1},func:e,args:[n]}).then()}))})();
